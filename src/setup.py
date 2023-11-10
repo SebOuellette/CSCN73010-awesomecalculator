d73cbe704953338385552def2dc5435a6cf44bfa
SebOuellette <seb_ouellette@honeybeeks.net>
2023-11-10 16:39:50 -0500

    edit for github actions to run

--- src/setup.py
+++ src/setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="An awesome calculator package",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/dedreira/awesomecalculator",
+    url="https://github.com/SebOuellette/CSCN73010-awesomecalculator",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
